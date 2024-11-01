afde053ed0fe3c75ba7e1174a5bb8a280a40f215
isam chajia <isamchajiabeat.com>
2024-11-01 16:05:46 +0100

    ms

---
+++ myinvoice.js
@@ -0,0 +1,40 @@
+// var :
+let itemTotal = document.querySelectorAll('.itemTotal');
+
+const invoiceTotal = document.getElementById('invoiceTotal');
+
+// tbody :
+const invoiceTable = document.getElementById('invoiceTable').getElementsByTagName('tbody')[0];
+
+let tr = invoiceTable.querySelectorAll('tr');
+
+
+// item total :
+let totalFacture = 0;
+for (let i = 0; i < tr.length; i++) {
+
+  let inputs = tr[i].querySelectorAll("input");
+
+  addEventListener('input', function(){
+
+    let tt = inputs[0].value * inputs[1].value;
+  
+    itemTotal[i].textContent = tt.toFixed(2);
+
+    totalFacture = totalFacture + Number(itemTotal[i].textContent);
+
+    // console.log(Number(itemTotal[i].textContent));
+
+    
+
+    invoiceTotal.textContent = totalFacture.toFixed(2);
+
+  });
+
+};
+
+// total 
+
+// add tr in table :
+
+
