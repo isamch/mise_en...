706165d0b25b8a570489249e23601a309a203d21
isam chajia <isamchajiabeat.com>
2024-11-01 19:56:02 +0100

    fun add

--- myinvoice.js
+++ myinvoice.js
@@ -1,7 +1,7 @@
 // var :
 let itemTotal = document.querySelectorAll('.itemTotal');
 
-const invoiceTotal = document.getElementById('invoiceTotal');
+let invoiceTotal = document.getElementById('invoiceTotal');
 
 // tbody :
 const invoiceTable = document.getElementById('invoiceTable').getElementsByTagName('tbody')[0];
@@ -9,43 +9,87 @@ const invoiceTable = document.getElementById('invoiceTable').getElementsByTagNam
 let tr = invoiceTable.querySelectorAll('tr');
 
 
-// item total :
-for (let i = 0; i < tr.length; i++) {
 
-  let inputs = tr[i].querySelectorAll("input");
 
-  addEventListener('input', function(){
 
-    let tt = inputs[0].value * inputs[1].value;
+  tr = invoiceTable.querySelectorAll('tr');
+
+  // item total :
+  for (let i = 0; i < tr.length; i++) {
   
-    itemTotal[i].textContent = tt.toFixed(2);
-
-    // total :
-    let totalFacture = 0;
-    for (let index = 0; index < tr.length; index++) {
-      totalFacture = totalFacture + Number(itemTotal[index].textContent);
-      invoiceTotal.textContent = totalFacture.toFixed(2);
-    }
-
-
-
+  
+    let inputs = tr[i].querySelectorAll("input");
+  
+    addEventListener('input', function(){
+  
+      let tt = inputs[0].value * inputs[1].value;
     
-  });
+      itemTotal[i].textContent = tt.toFixed(2);
+  
+      // total :
+      let totalFacture = 0;
+      for (let index = 0; index < tr.length; index++) {
+        totalFacture = totalFacture + Number(itemTotal[index].textContent);
+        invoiceTotal.textContent = totalFacture.toFixed(2);
+      }
+      
+    });
+  
+  };
 
-};
 
-// // add tr in table :
-// function addNewItem(){
-//   invoiceTable.innerHTML += `
-//      <tr>
-//                 <td>Article 3</td>
-//                 <td><input type="number" value="3" class="quantity" min="0"></td>
-//                 <td><input type="number" value="7.5" class="unitPrice" min="0" step="0.01"></td>
-//                 <td class="itemTotal">22.50</td>
-//             </tr>
-//   `;
 
-// };
+// add here :
+const btnAdd = document.querySelector(".add-button");
+var count = 4;
+btnAdd.addEventListener('click', function(){
+  invoiceTable.innerHTML += `
+  <tr>
+  <td>Article ${count}</td>
+  <td><input type="number" value="1" class="quantity" min="0"></td>
+  <td><input type="number" value="5" class="unitPrice" min="0" step="0.01"></td>
+  <td class="itemTotal">10.00</td>
+  </tr>
+  
+  `;
+  count++;
+
+
+  tr = invoiceTable.querySelectorAll('tr');
+  itemTotal = document.querySelectorAll('.itemTotal');
+
+    // item total :
+    for (let i = 0; i < tr.length; i++) {
+  
+  
+      let inputs = tr[i].querySelectorAll("input");
+    
+      addEventListener('input', function(){
+    
+        let tt = inputs[0].value * inputs[1].value;
+      
+        itemTotal[i].textContent = tt.toFixed(2);
+    
+        // total :
+        let totalFacture = 0;
+        for (let index = 0; index < tr.length; index++) {
+          totalFacture = totalFacture + Number(itemTotal[index].textContent);
+          invoiceTotal.textContent = totalFacture.toFixed(2);
+        }
+        
+      });
+    
+    };
+
+  
+  
+});
+
+
+
+
+
+
 
 
 
